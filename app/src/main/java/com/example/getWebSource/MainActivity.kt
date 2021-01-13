6ce7b54f8a4e9adeecc7639a1da30fe8411052ef
mauraqp <mauraqoonitah@gmail.com>
2021-01-13 14:10:49 +0700

    spinner update

--- app/src/main/java/com/example/getWebSource/MainActivity.kt
+++ app/src/main/java/com/example/getWebSource/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val myName: Words = Words("Maura Qoonitah")
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
@@ -25,8 +24,6 @@ class MainActivity : AppCompatActivity() {
         url_text.setOnClickListener {
             updateUrlname(it)
         }
-//        fun clickHandlerFunction(view: View) {
-//        addNickname(view)
 
         binding.doneButton.setOnClickListener {
             addUrlname(it)
@@ -40,6 +37,7 @@ class MainActivity : AppCompatActivity() {
             val adapter = ArrayAdapter(
                 this, android.R.layout.simple_spinner_item, choose
             )
+            adapter.setDropDownViewResource(R.layout.spinner_item)
             spinner.adapter = adapter
 
             spinner.onItemSelectedListener = object :  AdapterView.OnItemClickListener,
@@ -66,32 +64,19 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun addUrlname(view: View) {
-//        val editText = findViewById<EditText>(R.id.nickname_edit)
-//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
     binding.apply {
-//        nicknameTextView.text = editText.text
-        // convert the Editable to a String
-//        binding.nicknameText.text = binding.nicknameEdit.text.toString()
         myName.urlInput = url_edit.text.toString()
-
-//        editText.visibility = View.GONE
         url_edit.visibility = View.GONE
-
-//        view.visibility = View.GONE
         doneButton.visibility = View.GONE
-
-//        nicknameTextView.visibility = View.VISIBLE
         urlText.visibility = View.VISIBLE
         invalidateAll()
-
     }
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
-
     private fun updateUrlname (view: View) {
         val editText = findViewById<EditText>(R.id.url_edit)
         val doneButton = findViewById<Button>(R.id.done_button)
